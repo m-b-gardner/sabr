cf6ec514b431a8cf102aee4e98c31a60c45392e6
Matthew Gardner <matthew.brian.gardner@gmail.com>
2016-04-24 02:55:22 -0500

    completed figure coded mostly done
    more examples, weighting framework, on list

--- senior_pj_scrap.R
+++ senior_pj_scrap.R
@@ -79,6 +79,12 @@ samp2010 <- chart_div(ichiro2010, 30, "K")
 ichiro2011 <- sub_set(dat2011, "suzui001")
 ichiro2011$K <- alt_K(ichiro2011)
 ichiro2011 <- dates(ichiro2011)
+samp2011 <- chart_div(ichiro2011, 30, "K")
+
+ichiro2001 <- sub_set(dat2001, "suzui001")
+ichiro2001$K <- alt_K(ichiro2001)
+ichiro2001 <- dates(ichiro2001)
+samp2001 <- chart_div(ichiro2001, 30, "K")
 
 
 
@@ -90,17 +96,18 @@ ichiro.AB$HIT <- ifelse(ichiro.AB$H_FL > 0, 1, 0)
 ichiro.AB$DATE <- substr(ichiro.AB$GAME_ID, 4, 12)
 ichiro.AB <- ichiro.AB[order(ichiro.AB$DATE),]
 
-# using full data from 2010 
+# using 2010 data as the basis of the assumption 
 Dbar <- k0(sum(ichiro2010$K),length(ichiro2010$K))
 SD   <- sd  (samp2010)
 phat <- Dbar
 CL  <-  Dbar
-UCL <- CL + 3 * sqrt(phat * (1-phat) /(check+1))
-LCL <- max(c(-1e-5,CL - 3 * sqrt(phat * (1-phat) /(check+1))))
+UCL <- CL + 3 * sqrt(phat * (1-phat) /(length(samp2010)))
+LCL <- max(c(-1e-5,CL - 3 * sqrt(phat * (1-phat) /(length(samp2010)))))
 
 
-plot(1:(check+1), samp, main="Figure 1: Ichiro - K% (2011)", ylab="K%",
-     xlab="consecutive samples (size 30)", ylim=c(-0.02,0.35))
+plot(1:(length(samp2011)), samp2011, main="Figure 1: Ichiro - K% (2011) - 
+     Based on 2010", ylab="K%", xlab="consecutive samples (size 30)",
+     ylim=c(-0.02,0.35))
 abline(h=Dbar, lty=1, col="red", lwd=2)
 abline(h=UCL, lty=4, col="lightblue", lwd=2)
 abline(h=LCL, lty=4, col="lightblue", lwd=2)
@@ -108,29 +115,78 @@ legend("topleft", c("upper control limit = 30.82%","K% 2010 season average = 11.
                     ,"lower control limit = 0.00%"),lwd=c(2,2,2)
                     ,col=c("lightblue","red","lightblue"), lty=c(4,1,4))
 
-qqnorm(samp, ylab="K%", main="Normal Q-Q Plot: Ichiro - K% (2011)")
-
-
-# CUSUM looking for a shift of 1% K%
+# CUSUM looking for a shift of 1% K% (base 2010)
 k = 1/2
 h = 4
 K = k*SD
 H = h*SD
 Cp = c(0)
 Cm = c(0)
-for(i in 1:(check+1)){
-Cp[i] = max(0,samp[i]-(Dbar+K)+Cp[i-1])
-Cm[i] = min(0,(Dbar-K)-samp[i]+Cp[i-1])
+for(i in 1:(length(samp2011))){
+    Cp[i] = max(0,samp2011[i]-(Dbar+K)+Cp[i-1])
+    Cm[i] = min(0,(Dbar-K)-samp2011[i]+Cp[i-1])
 }  
 
-plot(1:(check+1), Cp, typ="l", col="red", ylim=c(-H-.05,H+.05), ylab="cumulative sum",lwd=2,
-     xlab = "consecutive samples (size 30)", main="Figure 2: CUSUM Control Chart - Ichiro - K% (2011)")
+plot(1:(length(samp2011)), Cp, typ="l", col="red", ylim=c(-H-.05,H+.05), ylab="cumulative sum",lwd=2,
+     xlab = "consecutive samples (size 30)", main="Figure 4: CUSUM Control Chart - Ichiro - K% (2011) - Based on 2010")
 par(new=T)
-plot(1:(check+1), Cm, typ="l", col="lightblue", axes=F, ylim=c(-H-.05,H+.05), ylab="", xlab="",lwd=2,)
-abline(h=H, lty=4, col="blue",lwd=2,)
-abline(h=-H, lty=4, col="blue",lwd=2,)
+plot(1:(length(samp2011)), Cm, typ="l", col="lightblue", axes=F, ylim=c(-H-.05,H+.05), ylab="", xlab="",lwd=2)
+abline(h=0, lty=4, col= "blue", lwd=2)
+abline(h=H, lty=4, col="blue",lwd=2)
+abline(h=-H, lty=4, col="blue",lwd=2)
 legend("topleft", c("upper limit","upper CUSUM","lower CUSUM","lower limit"),
        lwd=c(2,2,2,2)
        ,col=c("blue","red","lightblue","blue"), lty=c(4,1,1,4))
 par(new=F)
+
+# using 2001 data as the basis of the assumption (lazy coding by copying)
+Dbar <- k0(sum(ichiro2001$K),length(ichiro2001$K))
+SD   <- sd  (samp2001)
+phat <- Dbar
+CL  <-  Dbar
+UCL <- CL + 3 * sqrt(phat * (1-phat) /(length(samp2001)))
+LCL <- max(c(-1e-5,CL - 3 * sqrt(phat * (1-phat) /(length(samp2001)))))
+
+plot(1:(length(samp2011)), samp2011, main="Figure 2: Ichiro - K% (2011) -
+     Based on 2001", ylab="K%", xlab="consecutive samples (size 30)", 
+     ylim=c(-0.02,0.35))
+abline(h=Dbar, lty=1, col="red", lwd=2)
+abline(h=UCL, lty=4, col="lightblue", lwd=2)
+abline(h=LCL, lty=4, col="lightblue", lwd=2)
+legend("topleft", c("upper control limit = 22.48%","K% 2001 season average = 7.09%"
+                    ,"lower control limit = 0.00%"),lwd=c(2,2,2)
+       ,col=c("lightblue","red","lightblue"), lty=c(4,1,4))
+
+# CUSUM looking for a shift of 1% K% (base 2001)
+k = 1/2
+h = 4
+K = k*SD
+H = h*SD
+Cp = c(0)
+Cm = c(0)
+for(i in 1:(length(samp2011))){
+    Cp[i] = max(0,samp2011[i]-(Dbar+K)+Cp[i-1])
+    Cm[i] = min(0,(Dbar-K)-samp2011[i]+Cp[i-1])
+}  
+
+plot(1:(length(samp2011)), Cp, typ="l", col="red", ylim=c(-H-.05,H+.05), ylab="cumulative sum",lwd=2,
+     xlab = "consecutive samples (size 30)", main="Figure 5: CUSUM Control Chart - Ichiro - K% (2011) - Based on 2001")
+par(new=T)
+plot(1:(length(samp2011)), Cm, typ="l", col="lightblue", axes=F, ylim=c(-H-.05,H+.05), ylab="", xlab="",lwd=2)
+abline(h=0, lty=4, col= "blue", lwd=2)
+abline(h=H, lty=4, col="blue",lwd=2)
+abline(h=-H, lty=4, col="blue",lwd=2)
+legend("topleft", c("upper limit","upper CUSUM","lower CUSUM","lower limit"),
+       lwd=c(2,2,2,2)
+       ,col=c("blue","red","lightblue","blue"), lty=c(4,1,1,4))
+par(new=F)
+
+# Normality assumptions
+qqnorm(samp2011, ylab="K%", main="Figure 3: Normal Q-Q Plot: Ichiro - K% (2011)")
+qqline(samp2011)
+
+
+
+
+
 
