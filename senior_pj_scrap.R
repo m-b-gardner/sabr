4c5d926308601271370512f2b36f0575ed92a127
Matthew Gardner <matthew.brian.gardner@gmail.com>
2016-04-24 23:13:52 -0500

    final changes made
    could potentially come back and edit stuff, but this file will stay as the pair to the final senior paper

--- senior_pj_scrap.R
+++ senior_pj_scrap.R
@@ -1,7 +1,7 @@
 source("baseball_functions.R") # Personally written baseball functions
 library(devtools)
 source_gist(8892981)  # reads in parse.retrosheet2.pbp
-# parse.retrosheet2.pbp("2015") also read in for 2000-2014
+parse.retrosheet2.pbp("1999") #also read in for 2000-2014
 # manually move .csv files from folder to working directory
 
 # self-imposed limit on years
@@ -69,7 +69,7 @@ alt_K <- function(data){
 }
 
 
-
+#________________________________________________________________________
 # ichiro data
 ichiro2010 <- sub_set(dat2010, "suzui001")
 ichiro2010$K <- alt_K(ichiro2010)
@@ -86,16 +86,7 @@ ichiro2001$K <- alt_K(ichiro2001)
 ichiro2001 <- dates(ichiro2001)
 samp2001 <- chart_div(ichiro2001, 30, "K")
 
-
-
-
-
 # ichiro 2011 example
-ichiro.AB <- sub_set(dat2011, "suzui001", ab=TRUE)
-ichiro.AB$HIT <- ifelse(ichiro.AB$H_FL > 0, 1, 0)
-ichiro.AB$DATE <- substr(ichiro.AB$GAME_ID, 4, 12)
-ichiro.AB <- ichiro.AB[order(ichiro.AB$DATE),]
-
 # using 2010 data as the basis of the assumption 
 Dbar <- k0(sum(ichiro2010$K),length(ichiro2010$K))
 SD   <- sd  (samp2010)
@@ -112,14 +103,18 @@ abline(h=Dbar, lty=1, col="red", lwd=2)
 abline(h=UCL, lty=4, col="lightblue", lwd=2)
 abline(h=LCL, lty=4, col="lightblue", lwd=2)
 legend("topleft", c("upper control limit = 30.82%","K% 2010 season average = 11.61%"
-                    ,"lower control limit = 0.00%"),lwd=c(2,2,2)
-                    ,col=c("lightblue","red","lightblue"), lty=c(4,1,4))
+                    ,"lower control limit = 0.00%"),lwd=c(2,2,2), cex=0.75
+                    ,col=c("lightblue","red","lightblue"), lty=c(4,1,4),
+       box.lwd = 0,box.col = "white",bg = "white")
+box(which = "plot", lty = "solid")
 
 # CUSUM looking for a shift of 1% K% (base 2010)
 k = 1/2
-h = 4
+h = 5
 K = k*SD
 H = h*SD
+ARL1 = (exp(-2*(0-k)*(h+1.166))+2*(0-k)*(h+1.166)-1)/(2*(0-k)^2)
+(ARL = (1/ARL1+1/ARL1)^(-1))
 Cp = c(0)
 Cm = c(0)
 for(i in 1:(length(samp2011))){
@@ -128,15 +123,17 @@ for(i in 1:(length(samp2011))){
 }  
 
 plot(1:(length(samp2011)), Cp, typ="l", col="red", ylim=c(-H-.05,H+.05), ylab="cumulative sum",lwd=2,
-     xlab = "consecutive samples (size 30)", main="Figure 4: CUSUM Control Chart - Ichiro - K% (2011) - Based on 2010")
+     xlab = "consecutive samples (size 30)", main="Figure 4: CUSUM - Ichiro - K% (2011) - Based on 2010")
 par(new=T)
 plot(1:(length(samp2011)), Cm, typ="l", col="lightblue", axes=F, ylim=c(-H-.05,H+.05), ylab="", xlab="",lwd=2)
 abline(h=0, lty=4, col= "blue", lwd=2)
 abline(h=H, lty=4, col="blue",lwd=2)
 abline(h=-H, lty=4, col="blue",lwd=2)
 legend("topleft", c("upper limit","upper CUSUM","lower CUSUM","lower limit"),
-       lwd=c(2,2,2,2)
-       ,col=c("blue","red","lightblue","blue"), lty=c(4,1,1,4))
+       lwd=c(2,2,2,2), cex=0.75
+       ,col=c("blue","red","lightblue","blue"), lty=c(4,1,1,4),
+       box.lwd = 0,box.col = "white",bg = "white")
+box(which = "plot", lty = "solid")
 par(new=F)
 
 # using 2001 data as the basis of the assumption (lazy coding by copying)
@@ -154,14 +151,18 @@ abline(h=Dbar, lty=1, col="red", lwd=2)
 abline(h=UCL, lty=4, col="lightblue", lwd=2)
 abline(h=LCL, lty=4, col="lightblue", lwd=2)
 legend("topleft", c("upper control limit = 22.48%","K% 2001 season average = 7.09%"
-                    ,"lower control limit = 0.00%"),lwd=c(2,2,2)
-       ,col=c("lightblue","red","lightblue"), lty=c(4,1,4))
+                    ,"lower control limit = 0.00%"),lwd=c(2,2,2), cex=0.75
+       ,col=c("lightblue","red","lightblue"), lty=c(4,1,4),
+       box.lwd = 0,box.col = "white",bg = "white")
+box(which = "plot", lty = "solid")
 
 # CUSUM looking for a shift of 1% K% (base 2001)
 k = 1/2
-h = 4
+h = 5
 K = k*SD
 H = h*SD
+ARL1 = (exp(-2*(0-k)*(h+1.166))+2*(0-k)*(h+1.166)-1)/(2*(0-k)^2)
+(ARL = (1/ARL1+1/ARL1)^(-1))
 Cp = c(0)
 Cm = c(0)
 for(i in 1:(length(samp2011))){
@@ -170,23 +171,108 @@ for(i in 1:(length(samp2011))){
 }  
 
 plot(1:(length(samp2011)), Cp, typ="l", col="red", ylim=c(-H-.05,H+.05), ylab="cumulative sum",lwd=2,
-     xlab = "consecutive samples (size 30)", main="Figure 5: CUSUM Control Chart - Ichiro - K% (2011) - Based on 2001")
+     xlab = "consecutive samples (size 30)", main="Figure 5: CUSUM - Ichiro - K% (2011) - Based on 2001")
 par(new=T)
 plot(1:(length(samp2011)), Cm, typ="l", col="lightblue", axes=F, ylim=c(-H-.05,H+.05), ylab="", xlab="",lwd=2)
 abline(h=0, lty=4, col= "blue", lwd=2)
 abline(h=H, lty=4, col="blue",lwd=2)
 abline(h=-H, lty=4, col="blue",lwd=2)
 legend("topleft", c("upper limit","upper CUSUM","lower CUSUM","lower limit"),
-       lwd=c(2,2,2,2)
-       ,col=c("blue","red","lightblue","blue"), lty=c(4,1,1,4))
+       lwd=c(2,2,2,2), cex=.75
+       ,col=c("blue","red","lightblue","blue"), lty=c(4,1,1,4),
+       box.lwd = 0,box.col = "white",bg = "white")
+box(which = "plot", lty = "solid")
 par(new=F)
 
 # Normality assumptions
 qqnorm(samp2011, ylab="K%", main="Figure 3: Normal Q-Q Plot: Ichiro - K% (2011)")
 qqline(samp2011)
+shapiro.test(samp2011)
+#_______________________________________________________________________________
 
 
+# Showing differences in rational sample groups
+# Sorry for the lazy coding, this was a late decision to add
 
+# ichiro data
+samp2010 <- chart_div(ichiro2010, 20, "K")
+samp2011 <- chart_div(ichiro2011, 20, "K")
+samp2001 <- chart_div(ichiro2001, 20, "K")
 
+# ichiro 2011 example
+# using 2010 data as the basis of the assumption 
+Dbar <- k0(sum(ichiro2010$K),length(ichiro2010$K))
+SD   <- sd  (samp2010)
+phat <- Dbar
+CL  <-  Dbar
+UCL <- CL + 3 * sqrt(phat * (1-phat) /(length(samp2010)))
+LCL <- max(c(-1e-5,CL - 3 * sqrt(phat * (1-phat) /(length(samp2010)))))
 
+par(mfrow=c(2,2))
+plot(1:(length(samp2011)), samp2011, main="Figure 1", ylab="K%", xlab="consecutive samples (size 20)",
+     ylim=c(-0.02,0.35))
+abline(h=Dbar, lty=1, col="red", lwd=2)
+abline(h=UCL, lty=4, col="lightblue", lwd=2)
+abline(h=LCL, lty=4, col="lightblue", lwd=2)
+
+# CUSUM looking for a shift of 1% K% (base 2010)
+k = 1/2
+h = 5
+K = k*SD
+H = h*SD
+ARL1 = (exp(-2*(0-k)*(h+1.166))+2*(0-k)*(h+1.166)-1)/(2*(0-k)^2)
+(ARL = (1/ARL1+1/ARL1)^(-1))
+Cp = c(0)
+Cm = c(0)
+for(i in 1:(length(samp2011))){
+    Cp[i] = max(0,samp2011[i]-(Dbar+K)+Cp[i-1])
+    Cm[i] = min(0,(Dbar-K)-samp2011[i]+Cp[i-1])
+}  
+
+plot(1:(length(samp2011)), Cp, typ="l", col="red", ylim=c(-H-.05,H+.05), ylab="cumulative sum",lwd=2,
+     xlab = "consecutive samples (size 20)", main="Figure 4")
+par(new=T)
+plot(1:(length(samp2011)), Cm, typ="l", col="lightblue", axes=F, ylim=c(-H-.05,H+.05), ylab="", xlab="",lwd=2)
+abline(h=0, lty=4, col= "blue", lwd=2)
+abline(h=H, lty=4, col="blue",lwd=2)
+abline(h=-H, lty=4, col="blue",lwd=2)
+par(new=F)
+
+# using 2001 data as the basis of the assumption (lazy coding by copying)
+Dbar <- k0(sum(ichiro2001$K),length(ichiro2001$K))
+SD   <- sd  (samp2001)
+phat <- Dbar
+CL  <-  Dbar
+UCL <- CL + 3 * sqrt(phat * (1-phat) /(length(samp2001)))
+LCL <- max(c(-1e-5,CL - 3 * sqrt(phat * (1-phat) /(length(samp2001)))))
+
+plot(1:(length(samp2011)), samp2011, main="Figure 2", ylab="K%", xlab="consecutive samples (size 20)", 
+     ylim=c(-0.02,0.35))
+abline(h=Dbar, lty=1, col="red", lwd=2)
+abline(h=UCL, lty=4, col="lightblue", lwd=2)
+abline(h=LCL, lty=4, col="lightblue", lwd=2)
+
+# CUSUM looking for a shift of 1% K% (base 2001)
+k = 1/2
+h = 5
+K = k*SD
+H = h*SD
+ARL1 = (exp(-2*(0-k)*(h+1.166))+2*(0-k)*(h+1.166)-1)/(2*(0-k)^2)
+(ARL = (1/ARL1+1/ARL1)^(-1))
+Cp = c(0)
+Cm = c(0)
+for(i in 1:(length(samp2011))){
+    Cp[i] = max(0,samp2011[i]-(Dbar+K)+Cp[i-1])
+    Cm[i] = min(0,(Dbar-K)-samp2011[i]+Cp[i-1])
+}  
+
+plot(1:(length(samp2011)), Cp, typ="l", col="red", ylim=c(-H-.05,H+.05), ylab="cumulative sum",lwd=2,
+     xlab = "consecutive samples (size 20)", main="Figure 5")
+par(new=T)
+plot(1:(length(samp2011)), Cm, typ="l", col="lightblue", axes=F, ylim=c(-H-.05,H+.05), ylab="", xlab="",lwd=2)
+abline(h=0, lty=4, col= "blue", lwd=2)
+abline(h=H, lty=4, col="blue",lwd=2)
+abline(h=-H, lty=4, col="blue",lwd=2)
+title("Figure 6: Figures With Different Rational Groups", line=-1, outer=TRUE)
+par(new=F, mfrow=c(1,1))
 
